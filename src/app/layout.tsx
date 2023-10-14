a40a006996a29b7a89a2de088eaa24cc8c01cfdd
Diandra Prajoga <diandrajadeyomanda@gmail.com>
2023-10-14 22:04:20 +0700

    Updated folder structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Header from '@/components/header'
 import Footer from '@/components/footer'
-import { Providers } from '../provider'
+import { Providers } from './provider'
 import { ChakraProvider } from '@chakra-ui/react'
 
 const inter = Inter({ subsets: ['latin'] })
